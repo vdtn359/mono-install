dc88358938b643c4acf5d5d31de6e2107c84e295
Tuan Nguyen <vdtn359@gmail.com>
2020-03-24 23:15:30 +1100

    fix(tests): allow tests to pass

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const { compilerOptions } = require('./tsconfig');
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	modulePathIgnorePatterns: ['example'],
 	testMatch: ['<rootDir>/src/**/*.spec.ts'],
 	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
 		prefix: '<rootDir>/src/',
